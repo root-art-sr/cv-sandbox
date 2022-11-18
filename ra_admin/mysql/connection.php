eb3f613e16aeee000a59152129954be70f675e6a
root-art-sr <sascha@root-art.com>
2022-11-18 16:25:58 +0100

    db access off

--- ra_admin/mysql/connection.php
+++ ra_admin/mysql/connection.php
@@ -1,9 +1,8 @@
 <?php
 /// MySQL connections
-$connection = new mysqli("localhost", "sari_2", "353izXMandyLissa", "cv_sandbox") or die ("Connection failed");
+$connection = new mysqli("localhost", "***", "***", "cv_sandbox") or die ("Connection failed");
 if ($connection -> connect_errno) {
     echo "Failed to connect to MySQL: " . $connection -> connect_error;
     exit();
 }
 #sySAT
-?>
