415cd3eaf0204bd8665aaf2e7b85ffdda8c4ca9b
root-art-sr <sascha@root-art.com>
2022-11-18 21:26:07 +0100

    admin

--- ra_admin/index.php
+++ ra_admin/index.php
@@ -13,7 +13,6 @@ echo "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR
 <body>";
 // Started body
 // Main container
-# CKEditor
 echo "
 <div id='earth'>
     <!-- img src='../ra_data/images/ra_cms.png' alt='root-art_cms' width='120' height='100'><br -->
