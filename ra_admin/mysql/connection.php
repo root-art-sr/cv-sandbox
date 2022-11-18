86e8e7d78fea92fb7be6a3a989313d62e43499f2
root-art-sr <sascha@root-art.com>
2022-11-18 17:43:48 +0100

    ignore

--- ra_admin/mysql/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-/// MySQL connections
-$connection = new mysqli("localhost", "***", "***", "cv_sandbox") or die ("Connection failed");
-if ($connection -> connect_errno) {
-    echo "Failed to connect to MySQL: " . $connection -> connect_error;
-    exit();
-}
