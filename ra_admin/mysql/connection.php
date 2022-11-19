708af9208750cc98c7e63c760ec19631fbaf6b01
root-art-sr <sascha@root-art.com>
2022-11-19 15:52:13 +0100

    fixed untracked files

--- ra_admin/mysql/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-/// MySQL connections - replace your user and password accordingly
-$connection = new mysqli("localhost", "user", "password", "cv_sandbox") or die ("Connection failed");
-if ($connection -> connect_errno) {
-    echo "Failed to connect to MySQL: " . $connection -> connect_error;
-    exit();
-}
