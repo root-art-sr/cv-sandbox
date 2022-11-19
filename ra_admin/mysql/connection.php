af3ef1232f43389ceb2526f611a975f1d3794a33
root-art-sr <sascha@root-art.com>
2022-11-19 15:45:52 +0100

    fixed untracked files

--- ra_admin/mysql/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-/// MySQL connections
-$connection = new mysqli("localhost", "root", "#izx353", "cv_sandbox") or die ("Connection failed");
-if ($connection -> connect_errno) {
-    echo "Failed to connect to MySQL: " . $connection -> connect_error;
-    exit();
-}
-#sySAT
