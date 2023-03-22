3fdbf7a72af034ae543d777a722d213533ab809f
root-art-sr <sascha@root-art.com>
2023-03-22 15:28:28 +0100

    New server fixes

--- includes/createpdf.php
+++ includes/createpdf.php
@@ -5,7 +5,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="shortcut icon" href="/favicon.png" type="image/png" />
 <!-- link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" -->
-<link rel="stylesheet/less" type="text/css" href="/local_less/main.less" />
+<link rel="stylesheet/less" type="text/css" href="/less/main.less" />
 <script src="/scripts/less.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script>
