8590346183bb6671dc4ede23259062d11853b7db
Sascha Riess <117679099+root-art-sr@users.noreply.github.com>
2023-03-22 15:48:12 +0100

    Add files via upload

---
+++ ra_admin/mysql/connection.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * MySQL connections:           replace your user and password accordingly
+ * The MySQL structure is at:   cv-sandbox/ra_admin/mysql/cv_sandbox.sql
+ * 
+ */
+
+$connection = new mysqli("localhost", "user", "password", "cv_sandbox") or die ("Connection failed");
+if ($connection -> connect_errno) {
+    echo "Failed to connect to MySQL: " . $connection -> connect_error;
+    exit();
+}
