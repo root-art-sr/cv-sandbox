0e436c937371f7c2050c2cdd03671630246bb74d
root-art-sr <sascha@root-art.com>
2023-06-14 16:38:12 +0200

    Continue transfer latest code from productive CV

--- includes/createpdf.php
+++ includes/createpdf.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>PDF DOM Reading: sascha_riess_cv_<?= $pdfname ?>.pdf</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link rel="shortcut icon" href="/favicon.png" type="image/png" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
-    <link rel="stylesheet/less" type="text/css" href="/less/main.less" />
-    <script src="/scripts/less.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous" async></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js" async></script>
-</head>
+    <head>
+        <title>PDF DOM Reading: sascha_riess_cv_<?= $pdfname ?>.pdf</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
+        <link rel="stylesheet/less" type="text/css" href="/less/main.less" />
+        <script src="/scripts/less.js"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous" async></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js" async></script>
+    </head>
     <body>
         <div id="canvas_div_pdf" style="width: 99%;">
             <?= $pdfcontent ?>
